aec7f1521bec97431e80e1794e647b28f9a881b1
Eric Jensen <magnesium38@gmail.com>
2017-02-10 18:51:38 -0600

    Use /usr/bin/time -v if it exists in run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-go build && time ./latinsquares $1 results/$1.txt
+
+go build
+if type /usr/bin/time &> /dev/null; then
+    (/usr/bin/time -v ./latinsquares $1 results/$1.txt) &> results/$1.time
+else
+    (time ./latinsquares $1 results/$1.txt) &> results/$1.time
+fi
+
+echo "Results output to results/$1.time"
